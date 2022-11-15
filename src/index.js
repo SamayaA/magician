c18f7d669dca85c566ca14aa09e2e651320d6087
SamayaA <asadullaevasamaya@gmail.com>
2022-11-15 20:22:18 +0300

    Move logic to getters

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
 class Specials {
   #attack;
 
+  #attackCell;
+
   #stoned;
+
+  #stonedCell;
 
   set attack(cellNum) {
     if (typeof cellNum === 'number') {
-      this.#attack = 100 - (cellNum - 1) * 10;
+      this.#attackCell = cellNum;
     }
   }
 
   get attack() {
+    this.#attack = this.#attackCell ? (100 - (this.#attackCell - 1) * 10) : undefined;
     return this.#attack;
   }
 
   set stoned(cellNum) {
-    if (typeof cellNum === 'number' && this.#attack !== undefined) {
-      this.#stoned = this.#attack - Math.log2(cellNum) * 5;
+    if (typeof cellNum === 'number' && this.attack !== undefined) {
+      this.#stonedCell = cellNum;
     }
   }
 
   get stoned() {
+    this.#stoned = this.#stonedCell ? this.attack - Math.log2(this.#stonedCell) * 5 : undefined;
     return this.#stoned;
   }
 }
